e1e747f3d69d4825ea469b7202aedfd344189e82
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-01-06 21:14:42 -0300

    Initial commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World React</h1>
+			<p className="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque a fugiat consectetur quas dignissimos dolorem officia, eveniet culpa earum? Recusandae veritatis consectetur nam incidunt excepturi! Ullam animi placeat ratione impedit.</p>
+		</div>
+	);
+}
+
+export default App;
